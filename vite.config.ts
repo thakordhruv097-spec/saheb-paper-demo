2fc89a7bed8db34d1f0931456aab41eafc869399
Dhruvkumar Niteshkumar Thakor <thakordhruv097@gmail.com>
2026-08-25 21:40:33 +0530

    chore: remove demo from base URL and package name

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/saheb-paper-demo/',
+  base: './',
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
